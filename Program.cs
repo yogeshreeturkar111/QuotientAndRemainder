a8f2cf867e2d5db20a57e54cbe44fecd3f52c532
yogeshreeturkar111 <110226741+yogeshreeturkar111@users.noreply.github.com>
2022-08-03 22:37:11 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int dividend = 68, divisor = 7;
+
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        Console.WriteLine("Dividend:{0} Divisor:{1}", dividend, divisor);
+        Console.WriteLine("Quotient = " + quotient);
+        Console.WriteLine("Remainder = " + remainder);
+        Console.ReadLine();
+    }
+}
